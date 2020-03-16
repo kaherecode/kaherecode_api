f8c526433ab3feac9362ed3e6f6141483efe2367
Mamadou Aliou Diallo <alioukahere@gmail.com>
2020-03-16 21:23:18 +0000

    <feat>(tutorials) CRUD on Tag and Topic resource

---
+++ src/Entity/Tag.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    public function __toString()
+    {
+        return $this->label;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = strtolower($label);
+
+        return $this;
+    }
+}
