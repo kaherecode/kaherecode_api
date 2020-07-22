8d4ebfec7475fe59753f0f0509899476e4baac28
Mamadou Aliou Diallo <alioukahere@gmail.com>
2020-07-22 16:31:00 +0000

    <fix>(users) adds user id to the token payload

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+/**
+ *
+ */
+class JWTCreatedListener
+{
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $payload['id'] = $user->getId();
+        $event->setData($payload);
+    }
+}
